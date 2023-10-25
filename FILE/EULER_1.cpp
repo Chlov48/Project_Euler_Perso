2c8af41269b08affc71999e29946d8645e7c934b
LUMEN <60232440+Chlov48@users.noreply.github.com>
2023-10-25 17:53:26 +0200

    Add files via upload

---
+++ FILE/EULER_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int sumOfMultiple(int lim){
+    int x;
+    for (int i=0, i<= lim, i++){
+        if ((i%5==0)||(i%3==0)){
+            x+=i;
+        }
+    }return x;
+}
+
+int main()
+{
+    cout << sumOfMultiple(10) << endl;
+    cout << sumOfMultiple(49) << endl;
+    cout << sumOfMultiple(1000) << endl;
+    cout << sumOfMultiple(8456) << endl;
+    cout << sumOfMultiple(19564) << endl;
+
+    return 0;
+}
